a9b2ba0ac766181425f596aab0f2bdb041eb973a
Ademola Quozeem <72305068+Quozeem@users.noreply.github.com>
2022-10-06 17:44:50 +0100

    Delete index2.js

--- src/index2.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './components/style.css';
-import BusinessCard from './components/Maincontent';
-
-
-ReactDOM.render(
-<BusinessCard/>,document.querySelector('.container'))
-
